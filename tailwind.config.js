e96990f6a967e3060be56f4aaa5fe8d4ea22f1c3
gor2em <grkem.squash99@gmail.com>
2021-10-13 11:05:40 +0300

    getirReactTailwind first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode:'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
